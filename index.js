6346040cad4ec0560828015c69c7a5d35eb5e7a1
Kolyaj <kolyaj@yandex.ru>
2012-03-28 15:02:09 +0400

    Реврайтинг урлов.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+var port = 8678;
+var configFile = 'config.txt';
+
+var rewrites = [];
+parseConfig(configFile);
+
+require('fs').watch(configFile, function() {
+    parseConfig(configFile);
+});
+
+
 require('proxy').createServer(function(url) {
+    for (var i = 0; i < rewrites.length; i++) {
+        if (rewrites[i].pattern.test(url)) {
+            return url.replace(rewrites[i].pattern, rewrites[i].replacement);
+        }
+    }
     return url;
-}).listen(8678);
+}).listen(port);
+
+
+function parseConfig(fname) {
+    require('config-parser').parse(fname, function(err, result) {
+        if (err) {
+            console.log('Config parse error: ' + err.message);
+            return;
+        }
+        rewrites = result;
+    });
+}
